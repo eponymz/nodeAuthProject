bbc05753abcade7d2f2570f8e69e5d1f19d6bda2
Ian Sabey <isabey@sofi.org>
2018-08-07 09:55:46 -0600

    completed layout views and basic login/register views as well as wired route handlers

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
